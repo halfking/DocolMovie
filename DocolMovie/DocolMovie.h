1220211a5015fb93b0ef0001c82362e9ad275854
XUTAO HUANG <xutaohuang@XUTAOdeMacBook-Pro.local>
2021-02-04 15:41:06 +0800

    init

---
+++ DocolMovie/DocolMovie.h
@@ -0,0 +1,18 @@
+//
+//  DocolMovie.h
+//  DocolMovie
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocolMovie.
+FOUNDATION_EXPORT double DocolMovieVersionNumber;
+
+//! Project version string for DocolMovie.
+FOUNDATION_EXPORT const unsigned char DocolMovieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocolMovie/PublicHeader.h>
+
+
